a12de2fd6e82ecbeee25abcb0a70b8e8df9e37ad
revas89 <chauhan.reva@gmail.com>
2015-05-22 17:43:08 -0400

    commit 05/22

---
+++ src/org/prep/SolutionremoveElements.java
@@ -0,0 +1,17 @@
+package org.prep;
+/*
+ * Remove all elements from a linked list of integers that have value val.
+
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5
+ */
+public class SolutionremoveElements {
+    public ListNode removeElements(ListNode head, int val) {
+    	
+    	
+    	
+    	
+        
+    }
+}
