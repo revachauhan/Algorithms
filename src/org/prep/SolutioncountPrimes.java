07c72da4e6df09ea85bd884b3b483370041f9ee0
revas89 <chauhan.reva@gmail.com>
2015-05-22 17:42:51 -0400

    commit 05/22

--- src/org/prep/SolutioncountPrimes.java
+++ src/org/prep/SolutioncountPrimes.java
@@ -3,19 +3,63 @@ package org.prep;
  * 
  */
 public class SolutioncountPrimes {
-    public int countPrimes(int n) {
+	public static void main(String[] args) {
+		System.out.println(countPrimes1(17));
+	}
+    public static int countPrimes(int n) {
+    	int count =0;
     	
-    	for(int i = 0; i <=n;i++)
-    		findPrime(n);
-    	
+    	for(int i = 2; i<=n;i++)
+    	{
+    		if(isPrime(i))
+    			count++;
+    		
+    		
+    	}
+    	return count;
     	
         
     }
-    private static int findPrime(int num)
-    {
-    	for()
+    private static boolean isPrime(int num)
+	{
+		for(int i =2; i <=Math.sqrt(num); i++)
+			{
+			if(num%i==0)
+				return false;
+				
+			}
+		return true;
+		
+	}
+    
+    public static int countPrimes1(int n) {
+    	if (n <= 2)
+    		return 0;
+     
+    	// init an array to track prime numbers
+    	boolean[] primes = new boolean[n];
+    	for (int i = 2; i < n; i++)
+    		primes[i] = true;
+     
+    	//for (int i = 2; i <= Math.sqrt(n - 1); i++) {
+    	for (int i = 2; i <= n-1; i++) {
+    		if (primes[i]) {
+    			for (int j = i + i; j < n; j += i)
+    				primes[j] = false;
+    		}
+    	}
+     
+    	int count = 0;
+    	for (int i = 2; i < n; i++) {
+    		if (primes[i])
+    			count++;
+    	}
     	
-    	
+    	return count;
     	
     }
+    
+    	
+    	
+    
 }
