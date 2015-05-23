779515047f16ffe5e9bf66ff68bbb84cfcef3984
revas89 <chauhan.reva@gmail.com>
2015-05-22 21:18:37 -0400

    commit 05/22

--- src/org/prep/SolutionremoveElements.java
+++ src/org/prep/SolutionremoveElements.java
@@ -7,10 +7,38 @@ Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
 Return: 1 --> 2 --> 3 --> 4 --> 5
  */
 public class SolutionremoveElements {
-    public ListNode removeElements(ListNode head, int val) {
+	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		//head.next = new ListNode(2);
+		ListNode  result = removeElements(head, 2);
+		while(result!=null)
+			{
+			System.out.println(result.toString());
+			result = result.next;
+			}
+		
+	}
+public  static ListNode removeElements(ListNode head, int val) {
     	
+    	if(head==null)
+    		return null;
     	
-    	
+    	ListNode current = new ListNode(0);
+    	current.next = head;
+    	ListNode temp = current;
+    	while(current!=null)
+    	{
+    		
+    		while(current.next!=null&&current.next.val==val)
+    		{
+    			current.next = current.next.next;
+    		}
+    		
+    		current = current.next;
+    		
+    		
+    	}
+    	return temp.next;
     	
         
     }
