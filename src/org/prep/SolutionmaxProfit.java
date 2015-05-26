c91db35cc3248f78d278b099a545dc24942bb361
revas89 <chauhan.reva@gmail.com>
2015-05-26 19:33:38 -0400

    commit 05/26

--- src/org/prep/SolutionmaxProfit.java
+++ src/org/prep/SolutionmaxProfit.java
@@ -8,6 +8,7 @@ public class SolutionmaxProfit {
     	int minElement = Integer.MAX_VALUE;
     	for(int i =0; i <prices.length;i++)
     	{
+    		
     		profit = Math.max(profit, prices[i]-minElement);
     		minElement = Math.min(prices[i], minElement);
     	
---
+++ src/org/prep/SolutionmaxProfitII.java
@@ -0,0 +1,20 @@
+package org.prep;
+
+public class SolutionmaxProfitII {
+
+
+	public int maxProfit(int[] prices) {
+		int profit =0;
+		//int minElement = prices[0];
+		for(int i =0; i<prices.length-1;i++)
+		{
+			if(prices[i+1]>prices[i])
+			{
+				
+				profit = profit+(prices[i+1]-prices[i]);
+			}
+
+		}
+		return profit;
+	}
+}
