d9a2cf658cf6c1b4c2a15c53e23dd0a444555a48
revas89 <chauhan.reva@gmail.com>
2015-06-20 00:37:42 -0400

    coommit

--- src/org/prep/SolutionHasPathSum.java
+++ src/org/prep/SolutionHasPathSum.java
@@ -1,5 +1,31 @@
 package org.prep;
 
-public class SolutionHasPathSum {
 
+ 
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+     TreeNode(int x) { val = x; }
+  }
+ 
+public class SolutionHasPathSum {
+    public boolean hasPathSum(TreeNode node, int target) {
+	   return	hasPathHelper(node, target, 0);
+		
+	}
+	
+	public boolean hasPathHelper(TreeNode node,int target,int sum)
+	{
+		if(node == null)
+			return false;
+		else
+		{	sum = sum+node.val;
+			if(node.left==null&&node.right==null)
+				if(sum == target)
+					return true;
+			return(hasPathHelper(node.left, target, sum)||hasPathHelper(node.right, target, sum));
+		}
+		
+	}
 }
